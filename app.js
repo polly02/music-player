860872cc2f2860767238c1f9edc8bc8dd4842ee8
polina <polina.zdanovich.02@mail.ru>
2022-12-01 00:45:50 +0300

    js functional and edits in layout

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-const play = document.querySelector(".play")
 const audio = document.createElement("audio")
+
+const play = document.querySelector(".play")
+const next = document.querySelector(".next")
+const prev = document.querySelector(".prev")
+const title = document.querySelector("h1")
+const singer = document.querySelector("h2")
+
+
 let flag = false
+
+
 
 const arr = [
     {
         src: "./music/1.mp3",
-        "title": "Unholy",
-        "singer": "Sam Smith",
-        "background": "background-image: url(./) "
+        title: "Unholy",
+        singer: "Sam Smith",
+        background: "background-image: url(./) "
     },
     {
         src: "./music/2.mp3",
-        "title": "Fall out",
-        "singer": "Louise Dowd"
+        title: "Fall out",
+        singer: "Louise Dowd"
     },
     {
         src: "./music/3.mp3",
-        "title": "You like game",
-        "singer": "OWEEK"
+        title: "You like game",
+        singer: "OWEEK"
     },
     {
         src: "./music/4.mp3",
-        "title": "All the lies",
-        "singer": "OWEEK"
+        title: "All the lies",
+        singer: "OWEEK"
     },
     {
         src: "./music/5.mp3",
-        "title": "Supergirl",
-        "singer": "Anna Naklab"
+        title: "Supergirl",
+        singer: "Anna Naklab"
     },
 ]
 
@@ -37,44 +46,42 @@ play.addEventListener("click", () => {
     currentSong()
 })
 
-document.querySelector(".next").addEventListener("click", () => {
+next.addEventListener("click", () => {
     if (indexCurrentSong === arr.length - 1) {
         indexCurrentSong = 0
     } else {
         indexCurrentSong++
     }
     changeSong()
-
 })
 
-document.querySelector(".prev").addEventListener("click", () => {
+prev.addEventListener("click", () => {
     if (indexCurrentSong === 0) {
         indexCurrentSong = arr.length - 1
     } else {
         indexCurrentSong--
     }
     changeSong()
-
 })
-
-function changeSong() {
-    audio.src = arr[indexCurrentSong].src
-    audio.play()
-}
 
 function currentSong() {
     audio.src = arr[indexCurrentSong].src
+    title.innerHTML = arr[indexCurrentSong].title
+    singer.innerHTML = arr[indexCurrentSong].singer
     if (flag === false) {
         audio.play()
-        play.innerHTML.style = "background-image: background-image: url(./assets/pauseBtn.svg), width: 38px, height: 38px"
-       
-        // play.textContent = "выключить"
+        play.style = "background-image: url(./assets/pauseBtn.svg)"
         flag = true
     } else {
         audio.pause()
-        play.innerHTML.style = "background-image: background-image: url(./assets/playBtn.svg), width: 38px, height: 38px"
-
-        // play.textContent = "включить"
+        play.style = "background-image: url(./assets/playBtn.svg)"
         flag = false
     }
+}
+
+function changeSong() {
+    audio.src = arr[indexCurrentSong].src
+    title.innerHTML = arr[indexCurrentSong].title
+    singer.innerHTML = arr[indexCurrentSong].singer
+    audio.play()
 }
