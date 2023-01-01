110908259163d8e408ec9be025270207f5f90420
polina <polina.zdanovich.02@mail.ru>
2023-01-01 17:38:46 +0300

    final edits

--- app.js
+++ app.js
@@ -1,62 +1,129 @@
-const audio = document.createElement("audio")
+const play = document.querySelector('.play'),
+    like = document.querySelector('.like'),
+    repeat = document.querySelector('.repeat-btn'),
+    title = document.getElementById('title'),
+    singer = document.getElementById('singer'),
+    image = document.querySelector('.img'),
+    line = document.querySelector('.line'),
+    lineTime = document.querySelector('.line-time'),
+    time = document.querySelector('.time'),
 
-const play = document.querySelector(".play")
-const next = document.querySelector(".next")
-const prev = document.querySelector(".prev")
-const title = document.querySelector("h1")
-const singer = document.querySelector("h2")
-const image = document.querySelector("img")
-const duration = document.querySelector(".line-duration")
-const line = document.querySelector('.line')
-const time = document.querySelector('.time')
+    arr = [
+        {
+            src: "./music/1.mp3",
+            title: "Unholy",
+            singer: "Sam Smith",
+            image: "./png/unholy.png",
+            likeFlag: false
+        },
+        {
+            src: "./music/2.mp3",
+            title: "Fall out",
+            singer: "Louise Dowd",
+            image: "./png/fallout.png",
+            likeFlag: false
+        },
+        {
+            src: "./music/3.mp3",
+            title: "You like game",
+            singer: "OWEEK",
+            image: "./png/oweek.png",
+            likeFlag: false
+        },
+        {
+            src: "./music/4.mp3",
+            title: "All the lies",
+            singer: "OWEEK",
+            image: "./png/oweek.png",
+            likeFlag: false
+        },
+        {
+            src: "./music/5.mp3",
+            title: "Supergirl",
+            singer: "Anna Naklab",
+            image: "./png/supergirl.png",
+            likeFlag: false
+        },
+    ]
 
-const arr = [
-    {
-        src: "./music/1.mp3",
-        title: "Unholy",
-        singer: "Sam Smith",
-        image: "./png/unholy.png"
-    },
-    {
-        src: "./music/2.mp3",
-        title: "Fall out",
-        singer: "Louise Dowd",
-        image: "./png/fallout.png"
-    },
-    {
-        src: "./music/3.mp3",
-        title: "You like game",
-        singer: "OWEEK",
-        image: "./png/oweek.png"
-    },
-    {
-        src: "./music/4.mp3",
-        title: "All the lies",
-        singer: "OWEEK",
-        image: "./png/oweek.png"
-    },
-    {
-        src: "./music/5.mp3",
-        title: "Supergirl",
-        singer: "Anna Naklab",
-        image: "./png/supergirl.png"
-    },
-]
+let indexCurrentSong = 0,
+    repeatFlag = false,
+    likeFlag = false,
+    flag = false;
 
-let flag = false
-let indexCurrentSong = 0
+const audioTag = document.createElement('audio')
 
-// audio.src = arr[indexCurrentSong].src
-// title.innerHTML = songs[indexCurrentSong].title;
-// singer.innerHTML = songs[indexCurrentSong].author;
-// image.src = songs[indexCurrentSong].image;
-// audioTag.src = songs[indexOfSong].src;
+image.src = arr[indexCurrentSong].image
+title.innerHTML = arr[indexCurrentSong].title
+singer.innerHTML = arr[indexCurrentSong].singer
 
-play.addEventListener("click", () => {
+play.addEventListener('click', () => {
     currentSong()
 })
 
-next.addEventListener("click", () => {
+document.querySelector('.next').addEventListener('click', () => {
+    if (indexCurrentSong === arr.length - 1) {
+        indexCurrentSong = 0
+    } else {
+        indexCurrentSong++
+    }
+
+    changeSong();
+});
+
+document.querySelector('.back').addEventListener('click', () => {
+    if (indexCurrentSong === 0) {
+        indexCurrentSong = arr.length - 1
+    } else {
+        indexCurrentSong--
+    }
+
+    changeSong()
+})
+
+function currentSong() {
+    audioTag.src = arr[indexCurrentSong].src
+    if (flag === false) {
+        play.style = 'background-image: url(./assets/pauseBtn.svg)';
+        audioTag.play()
+        flag = true
+    } else {
+        play.style = 'background-image: url(./assets/playBtn.svg)';
+        audioTag.pause()
+        flag = false
+    }
+}
+
+function changeSong() {
+    audioTag.src = arr[indexCurrentSong].src
+    image.style = ` background-image: url(${arr[indexCurrentSong].image})`
+    title.innerHTML = arr[indexCurrentSong].title
+    singer.innerHTML = arr[indexCurrentSong].singer
+    audioTag.play()
+    playFlag = true
+}
+
+function updateProgress(event) {
+    const {
+        duration,
+        currentTime
+    } = event.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    line.style.width = `${progressPercent}%`
+}
+audioTag.addEventListener('timeupdate', updateProgress)
+
+function setProgress(event) {
+    const width = this.clientWidth
+    const click = event.offsetX
+    const duration = audioTag.duration
+
+    audioTag.currentTime = (click / width) * duration
+}
+
+lineTime.addEventListener('click', setProgress)
+
+audioTag.addEventListener('ended', () => {
     if (indexCurrentSong === arr.length - 1) {
         indexCurrentSong = 0
     } else {
@@ -64,68 +131,3 @@ next.addEventListener("click", () => {
     }
     changeSong()
 })
-
-prev.addEventListener("click", () => {
-    if (indexCurrentSong === 0) {
-        indexCurrentSong = arr.length - 1
-    } else {
-        indexCurrentSong--
-    }
-    changeSong()
-})
-
-function currentSong() {
-    audio.src = arr[indexCurrentSong].src
-    title.innerHTML = arr[indexCurrentSong].title
-    singer.innerHTML = arr[indexCurrentSong].singer
-    image.src = arr[indexCurrentSong].image
-    if (flag === false) {
-        audio.play()
-        play.style = "background-image: url(./assets/pauseBtn.svg)"
-        flag = true
-    } else {
-        audio.pause()
-        play.style = "background-image: url(./assets/playBtn.svg)"
-        flag = false
-    }
-}
-
-function changeSong() {
-    audio.src = arr[indexCurrentSong].src
-    title.innerHTML = arr[indexCurrentSong].title
-    singer.innerHTML = arr[indexCurrentSong].singer
-    image.src = arr[indexCurrentSong].image
-    audio.play()
-}
-
-audio.addEventListener('timeupdate', (event) => {
-    const duration = event.target.duration;
-    const currentTime = event.target.currentTime;
-    const progressPercent = (currentTime / duration) * 100;
-    line.style.width = `${progressPercent}%`;
-
-    let currentMin = Math.floor(currentTime / 60);
-    let currentSec = Math.floor(currentTime % 60);
-    if (currentMin < 10) currentMin = `0${currentMin}`;
-    if (currentSec < 10) currentSec = `0${currentSec}`;
-    time.innerText = `${currentMin}:${currentSec}`;
-});
-
-function setProgress(event) {
-    const width = this.clientWidth;
-    const clickLineProgress = event.offsetX;
-    const duration = audio.duration;
-
-    audio.currentTime = (clickLineProgress / width) * duration;
-}
-
-duration.addEventListener('click', setProgress);
-
-audio.addEventListener('ended', () => {
-    if (indexCurrentSong === arr.length - 1) {
-        indexCurrentSong = 0;
-    } else {
-        indexCurrentSong++;
-    }
-    changeSong();
-});
